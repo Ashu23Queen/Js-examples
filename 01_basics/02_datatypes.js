b09e4bb22c08c499fab787eec2ddd8fbdd5f9609
Ashu23Queen <165048198+Ashu23Queen@users.noreply.github.com>
2024-05-28 07:32:13 +0000

    datatypes basics & examles

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,24 @@
+"use strict" // all js code will un as newer version
+
+//alert(5 + 9) it will throw error as its node and not browser...
+
+console.log(5 + 5)
+console.log("Ashwini")
+
+//all types of datatypes
+//number 
+//string
+//boolean
+//null
+//undefined
+//symbol 
+
+//object example
+
+console.log(typeof "Ashwini Shinde")
+console.log(typeof null)
+
+
+//evaluate epressions using datatype
+let x = "Volvo" + 16 + 4;
+console.log(x)     //When adding a number and a string, JavaScript will treat the number as a string.
