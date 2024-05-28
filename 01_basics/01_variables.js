1575de3a84c55f18b37ec41191d32c12e55d56d1
Ashu23Queen <165048198+Ashu23Queen@users.noreply.github.com>
2024-05-28 06:54:22 +0000

    Study variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 1556877
+let accountEmail = "ashu@google.com"
+var accountPassword = "57896"
+accountCity = "Mumbai"
+let accountState;
+//accountId = 2 not allowed to change as it is constant variable 
+
+accountEmail = "abc@gmail.com"
+accountPassword = "78925"
+accountCity = "Pune"
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
