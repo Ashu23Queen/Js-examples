1471d353b798e0914a991e200ebce9bf0a0c3d87
Ashu23Queen <165048198+Ashu23Queen@users.noreply.github.com>
2024-06-02 04:43:17 +0000

    array_examples

---
+++ 01_basics/05_arrays.js
@@ -0,0 +1,14 @@
+
+
+//how to get the length of array
+const cars = ["Toyota Camry", "Honda CR-V", "Ford F-Series", "Hyundai Tucson"];
+let lengthOfArray = cars.length;
+
+console.log(lengthOfArray);
+
+
+//how to convert arrays into strings
+const cars1 = ["Toyota Camry", "Honda CR-V", "Ford F-Series", "Hyundai Tucson"];
+let carsInToString = cars1.toString()
+console.log(carsInToString);
+ 
