4bc7b662d9cdfed1ceff94e2e5b90ad84b0f424c
Ashu23Queen <165048198+Ashu23Queen@users.noreply.github.com>
2024-06-04 07:55:21 +0000

    creating objects, retrieving values from respective object...

---
+++ 01_basics/07_objects.js
@@ -0,0 +1,8 @@
+
+//retrieving values from Object
+const flowers  = {type:"Tulip", color:"yellow", season:"spring "};
+// Display Data from the Object given
+let answer = flowers .season;
+console.log(answer);
+
+ 
