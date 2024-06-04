f85d21de1fd65d16d750908d996b3c464a54cb7f
Ashu23Queen <165048198+Ashu23Queen@users.noreply.github.com>
2024-06-04 08:27:25 +0000

    Create a JavaScript Sets using recipes list as a example

--- 01_basics/07_objects.js
+++ 01_basics/07_objects.js
@@ -17,3 +17,17 @@ const person = {
   
 let answer1 = person.firstname + " is " + person.age;
 console.log(answer1);
+
+
+
+// Create a JavaScript Sets using recipes list as a example
+const recipe = new Set();
+
+// Add Values to the list
+recipe.add("Spanish Custard");
+recipe.add("Pan-Fried Halloumi");
+recipe.add("Loaded Kimchi Fries");
+
+// Displaying the recipes
+let recipeAnswer = "The list has " + recipe.size + " recipes.";
+console.log(recipeAnswer);
